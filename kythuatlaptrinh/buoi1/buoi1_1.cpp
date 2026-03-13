1050adbe710599dfc58b9bf04258d22fd032a01d
PMT201 <PMT201@PMT3025>
2026-03-13 11:09:26 +0700

    buoi1-1

--- kythuatlaptrinh/buoi1/buoi1_1.cpp
+++ kythuatlaptrinh/buoi1/buoi1_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 struct person {
@@ -9,7 +10,47 @@ struct person {
 	string address;
 
 };
-void viewpersonlist(vector<person)
+void viewpersonlist(vector<person> p) {
+	if (p.size() == 0)
+		cout << "an emty list" << endl;
+	else {
+		for (int i = 0; i < p.size(); i++) {
+			cout << "person" << (i + 1) << endl;
+			cout << "\t+ id:" << p[i].id << endl;
+			cout << "\t+ name: " << p[i].name << endl;
+			cout << "\t+ age: " << p[i].age << endl;
+			cout << "\t+ address: " << p[i].address << endl;
+			
+
+		}
+	}
+}
+void removeperson(vector<person>& p, int id) {
+	for (auto i = p.begin(); i != p.end(); i++) {
+		if (i->id == id) {
+			p.erase(i);
+			cout << "remove a person sucsessfully" << endl;
+			return;
+		}
+	}
+	cout << "not found person with id" << id << endl;
+}
+
+void addperson(vector<person>& p) {
+	person a;
+	cout << "+ id: ";
+	cin >> a.id;
+	cout << "+ name: ";
+	cin.ignore();
+	getline(cin, a.name);
+	cout << "+ age: ";
+	cin >> a.age;
+	cout << "+ address: ";
+	cin.ignore();
+	getline(cin, a.address);
+	p.push_back(a);
+	cout << "add a person sucsessfully" << endl;
+}
 int main() {
 	vector<person> list;
 	do {
@@ -29,14 +70,17 @@ int main() {
 		switch (cmd)
 		{
 		case 1: {
-			break;
+			viewpersonlist(list); break;
 		}
 		case 2: {
-			break;
+			addperson(list); break;
 
 		}
 		case 3: {
-			break;
+			int id;
+			cout << "input id to remove: ";
+			cin >> id;
+			removeperson(list, id); break;
 		}
 		case 4: {
 			break;
