8a32b13a002cd31d22dcc19e74802bea8c4a472b
Administrator <Administrator@PMT2025>
2026-04-20 13:41:49 +0700

    .

--- kythuatlaptrinh/buoi7/Account.h
+++ kythuatlaptrinh/buoi7/Account.h
@@ -6,7 +6,23 @@ struct Account {
 		os << "Account information: " << endl;
 		os << "\t+ Link: " << b.lnk << endl;
 		os << "\t+ User Name: " << b.usr << endl;
-		os << "\t+ Author name: " << b.author.name << endl;
+		os << "\t+ Passord: " << b.pwd << endl;
+		os << "\t+ Description: " << b.desc << endl;
 		return os;
 	}
+	friend ostream& operator<<(ostream& os, Account& b) {
+		cout << "Account information: " << endl;
+		cout << "\t+ Id: ";
+		in >> b.id
+		cout << "\t+ Link: ";
+		in.ignore();
+		getline(in, b.lnk);
+		cout << "\t+ User name: ";
+		in.ignore();
+		getline(in, b.usr);
+		cout << "\t+ Password: ";
+		getline(in, b.pwd);
+		cout << "\t+ Description: ";
+		getline(in, b.desc);
+		return in;
 };
--- kythuatlaptrinh/buoi7/buoi7.cpp
+++ kythuatlaptrinh/buoi7/buoi7.cpp
@@ -26,58 +26,30 @@ int main()
 		switch (choice)
 		{
 		case 1: {
-			books.Show();
-			break;
+
 		}
 		case 2: {
-			Book b;
-			cin >> b;
-			Node* newNode = new Node;
-			newNode->Create(b);
-			books.AddFirst(newNode);
-			break;
+			
 
 		}
 		case 3: {
 			int removeId;
-			cout << "enter book's id to remove:  ";
-			cin >> removeId;
-			bool res = books.Remove(removeId);
-			if (res)
-				cout << "remove book successfully" << endl;
-			else
-				cout << "Invalid book id" << endl;
+			cout << "enter account id to remove: "
+				cin >> removeId;
+			account.Remove(removeId);
 			break;
 		}
 		case 4: {
-			int updateId;
-			cout << "enter book id to update: ";
-			cin >> updateId;
-			bool res = books.Update(updateId);
-			if (res)
-				cout << "enter book successfully" << endl;
-			else
-				cout << "invalid book id" << endl;
+			
 			break;
 
 		}
 		case 5: {
-			string bookName;
-			cout << "enter a books name to find: ";
-			cin.ignore();
-			getline(cin, bookName);
-			Book* res = books.Find(bookName);
-			if (res != NULL) {
-				cout << *res;
-			}
-			else {
-				cout << "no books with name: " << bookName << endl;
-			}
+			
 			break;
 		}
 		case 6: {
-			books.Export("25TH1.dla");
-			cout << "Exported successfully" << endl;
+			
 			break;
 		}
 		case 0: {
